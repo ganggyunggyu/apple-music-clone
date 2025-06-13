915ebf192c588e9047783e2a80fb6c15f6f8399b
ganggyunggyu <qwzx16@naver.com>
2025-06-14 05:59:36 +0900

    chore: https add

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
+import fs from 'fs';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
+const cert = fs.readFileSync('localhost.pem');
+const key = fs.readFileSync('localhost-key.pem');
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@views', replacement: '/src/views' },
+      { find: '@resource', replacement: '/src/static/resource' },
+    ],
+  },
+  server: {
+    host: true,
+    https: {
+      key,
+      cert,
+    },
+  },
 });
