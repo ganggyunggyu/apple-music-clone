5dba253bdbfd7a51caa71ffb7bc63773ddc3c925
ganggyunggyu <qwzx16@naver.com>
2025-06-14 17:45:08 +0900

    feat: route animation add

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { GlassBottomBar } from '../components/glass-bottom-bar';
-import { Routeing } from '../pages';
+import { Routing } from '../pages';
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <Routeing />
+      <Routing />
       <GlassBottomBar />
     </BrowserRouter>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,48 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
+
+import { fadeVariants, slideVariants } from '@/app/motion';
+
 import { HomePage } from './home-page';
 import { RadioPage } from './radio-page';
 import { LibraryPage } from './library-page';
 import { SearchPage } from './search-page';
 
-export const Routeing = () => {
+export const Routing = () => {
+  const location = useLocation();
+
+  const allRoutes = [
+    { path: '/', element: <HomePage /> },
+    { path: '/radio', element: <RadioPage /> },
+    { path: '/library', element: <LibraryPage /> },
+    { path: '/search', element: <SearchPage /> },
+  ];
+
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/radio" element={<RadioPage />} />
-      <Route path="/library" element={<LibraryPage />} />
-      <Route path="/search" element={<SearchPage />} />
-    </Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        {allRoutes.map(({ path, element }) => {
+          const variants = path === '/search' ? slideVariants : fadeVariants;
+
+          return (
+            <Route
+              key={path}
+              path={path}
+              element={
+                <motion.div
+                  variants={variants}
+                  initial="initial"
+                  animate="animate"
+                  exit="exit"
+                  className="h-full w-full"
+                >
+                  {element}
+                </motion.div>
+              }
+            />
+          );
+        })}
+      </Routes>
+    </AnimatePresence>
   );
 };
